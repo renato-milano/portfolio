efe26f968f9ffcfaa42aa7754fadbb757bd22ac7
renato_milano <renatomilanodev@gmail.com>
2025-03-24 22:36:13 +0100

    boh veloce 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import NavBar from "./component/NavBar.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
