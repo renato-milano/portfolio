733f099457203983d8ec13db55ffa62224b442b1
renato-milano <renatomilanodev@gmail.com>
2025-03-31 13:57:12 +0200

    Footer font fix

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,11 +3,11 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-white py-20 px-4 text-center md:text-left">
+    <footer className="font-quick bg-black text-white py-20 px-4 text-center md:text-left">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
         {/* Logo o Nome */}
         <div>
-          <h2 className="text-xl font-bold">Renato Milano</h2>
+          <h2 className="font-quick text-xl">Renato Milano</h2>
           <p className="text-sm text-gray-400">Solution Developer.</p>
         </div>
         
@@ -20,11 +20,11 @@ export default function Footer() {
         
         {/* Dati Legali */}
         <div>
-          <h3 className="text-lg font-semibold">Dati Legali</h3>
-          <p className="text-sm text-gray-400">© 2025 Renato Milano. Tutti i diritti riservati.</p>
-          <p className="text-sm text-gray-400">P.IVA: 10779291219</p>
-          <p className="text-sm text-gray-400">Email: renatomilanodev@gmail.com</p>
-          <p className="text-sm text-gray-400">Pec: renatomilano@pec.it</p>
+          <h3 className="text-lg font-quick">Dati Legali</h3>
+          <p className="text-sm font-quick text-gray-400">© 2025 Renato Milano. Tutti i diritti riservati.</p>
+          <p className="text-sm font-quick text-gray-400">P.IVA: 10779291219</p>
+          <p className="text-sm font-quick text-gray-400">Email: renatomilanodev@gmail.com</p>
+          <p className="text-sm font-quick text-gray-400">Pec: renatomilano@pec.it</p>
         </div>
       </div>
     </footer>
